cef6e2741248dfa8f173cdcdbdb4f71917e77847
Zygimantas <zyg.norkus@gmail.com>
2023-12-08 16:34:52 +0200

    Complete Division

---
+++ java/part02-Part02_25.Division/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,14 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+
+        division(3, 5);
+
+    }
+
+    public static void division(int first, int second) {
+        System.out.println((double) first / second);
+    }
+}
