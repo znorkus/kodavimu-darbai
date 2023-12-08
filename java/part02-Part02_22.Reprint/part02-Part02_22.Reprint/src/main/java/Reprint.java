f46e2672ac70c32bc2a88c3bf94dd3b0a2d80b11
Zygimantas <zyg.norkus@gmail.com>
2023-12-08 15:01:06 +0200

    Fix formating

--- java/part02-Part02_22.Reprint/part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ java/part02-Part02_22.Reprint/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -7,12 +7,13 @@ public class Reprint {
         Scanner number = new Scanner(System.in);
         System.out.println("How many times?");
 
-         int times = Integer.valueOf(number.nextLine());
+        int times = Integer.valueOf(number.nextLine());
 
         for (int i = 1; i <= times; i++) {
             printText();
         }
     }
+
     public static void printText() {
         System.out.println("In a hole in the ground there lived a method");
     }
