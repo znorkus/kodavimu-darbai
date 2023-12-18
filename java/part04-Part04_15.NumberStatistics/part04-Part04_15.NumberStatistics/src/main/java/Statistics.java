c6b6e5a5036cbe5e946d27edf28d4e750d21701a
Zygimantas <zyg.norkus@gmail.com>
2023-12-18 14:45:41 +0200

    Do NumberStatistics

---
+++ java/part04-Part04_15.NumberStatistics/part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -0,0 +1,28 @@
+public class Statistics {
+    private int count;
+    private int sum;
+
+    public Statistics() {
+    }
+
+    public void addNumber(int number) {
+        sum = sum + number;
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int sum() {
+        return sum;
+    }
+
+    public double average() {
+        if (count == 0) {
+            return 0;
+        } else {
+            return (double) sum / count;
+        }
+    }
+}
