868276902e11989b09e1f692414fc2eb24d1319d
Zygimantas <zyg.norkus@gmail.com>
2023-12-19 11:35:17 +0200

    Do Book

---
+++ java/part04-Part04_20.Books/part04-Part04_20.Books/src/main/java/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+
+    private String title;
+    private int pages;
+    private int year;
+
+    public Book(String title, int pages, int year) {
+        this.title = title;
+        this.pages = pages;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.title + ", " + this.pages + " pages, " + this.year;
+    }
+}
