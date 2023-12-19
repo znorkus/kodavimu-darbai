18142fb391694526489bed4d9a70077c1fbea07b
Zygimantas <zyg.norkus@gmail.com>
2023-12-19 09:19:33 +0200

    Do PersonalInformationCollection

---
+++ java/part04-Part04_18.PersonalInformation/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PersonalInformationCollection {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the PersonalInformation class
+        ArrayList<PersonalInformation> persons = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+// Read person information from the user
+        while (true) {
+            System.out.print("First name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Identification number: ");
+            //String idNumber = scanner.nextLine();
+
+            //System.out.print("Enter the age of the person " + name + ": ");
+
+            String identificationNumber = String.valueOf(scanner.nextLine());
+            persons.add(new PersonalInformation(name, lastName, identificationNumber));
+        }
+        
+        System.out.println(" ");
+        for (PersonalInformation person : persons) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
+    }
+}
+//identificationNumber
