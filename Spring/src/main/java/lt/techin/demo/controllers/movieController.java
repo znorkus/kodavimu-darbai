bc2808dd477d032f0156ba4d1ce9f7c39b1388a3
Zygimantas <zyg.norkus@gmail.com>
2024-01-19 14:30:09 +0200

    Create getMovie method

--- Spring/src/main/java/lt/techin/demo/controllers/movieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/movieController.java
@@ -3,6 +3,7 @@ package lt.techin.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,11 +13,16 @@ import java.util.List;
 public class movieController {
 
     private ArrayList<String> movies = new ArrayList<>(
-            List.of("ToyStory", "A Bug's Life", "Toy Story 2", "Monsters, Inc.", "Finding Nemo"));
+            List.of("Toy Story", "A Bug's Life", "Toy Story 2", "Monsters, Inc.", "Finding Nemo"));
 
 
     @GetMapping("/movies")
     public ArrayList<String> getMovies() {
         return movies;
+    }
+
+    @GetMapping("/movies/{index}")
+    public String getMovie(@PathVariable int index) {
+        return this.movies.get(index);
     }
 }
