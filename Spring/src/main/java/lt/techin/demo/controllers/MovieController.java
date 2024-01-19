c1bfe912cf6c931b024abbcbb40ae81abac80b41
Zygimantas <zyg.norkus@gmail.com>
2024-01-19 15:49:15 +0200

    Update data types from String to Movie

--- Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
@@ -2,7 +2,6 @@ package lt.techin.demo.controllers;
 
 
 import lt.techin.demo.models.Movie;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +10,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class movieController {
+public class MovieController {
 
     private ArrayList<Movie> movies = new ArrayList<>(
-            List.of(new Movie(1L, "Toy Story", "John Lasseter", (short) 1995, (short) 81),
-                    new Movie(2L, "A Bug's Life", "John Lasseter", (short) 1998, (short) 81)));
+            List.of(new Movie(1L, "Toy Story", "John Lasseter",
+                            (short) 1995, (short) 81),
+                    new Movie(2L, "A Bug's Life", "John Lasseter",
+                            (short) 1998, (short) 81)));
 
 
     @GetMapping("/movies")
-    public ArrayList<String> getMovies() {
-        return movies;
+    public ArrayList<Movie> getMovies() {
+        return this.movies;
     }
 
     @GetMapping("/movies/{index}")
-    public String getMovie(@PathVariable int index) {
+    public Movie getMovie(@PathVariable int index) {
         return this.movies.get(index);
     }
 }
