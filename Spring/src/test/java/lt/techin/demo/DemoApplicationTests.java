ed6fb8b34d4c0ca60a76dca7d49584e2739da7fa
Zygimantas <zyg.norkus@gmail.com>
2024-01-19 10:00:53 +0200

    Create First Sping Boot Project

---
+++ Spring/src/test/java/lt/techin/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
