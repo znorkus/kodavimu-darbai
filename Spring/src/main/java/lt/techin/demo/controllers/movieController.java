e73621328274a2e57e08feeed6744d19bdff8277
Zygimantas <zyg.norkus@gmail.com>
2024-01-19 15:33:04 +0200

    Update ArrayList to use objects

--- Spring/src/main/java/lt/techin/demo/controllers/movieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/movieController.java
@@ -1,6 +1,7 @@
 package lt.techin.demo.controllers;
 
 
+import lt.techin.demo.models.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +13,9 @@ import java.util.List;
 @RestController
 public class movieController {
 
-    private ArrayList<String> movies = new ArrayList<>(
-            List.of("Toy Story", "A Bug's Life", "Toy Story 2", "Monsters, Inc.", "Finding Nemo"));
+    private ArrayList<Movie> movies = new ArrayList<>(
+            List.of(new Movie(1L, "Toy Story", "John Lasseter", (short) 1995, (short) 81),
+                    new Movie(2L, "A Bug's Life", "John Lasseter", (short) 1998, (short) 81)));
 
 
     @GetMapping("/movies")
