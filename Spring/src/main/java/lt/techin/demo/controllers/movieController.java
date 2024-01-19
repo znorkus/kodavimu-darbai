28fc6961192d3bad4c83ff5822ffd811e4b9febb
Zygimantas <zyg.norkus@gmail.com>
2024-01-19 13:42:55 +0200

    Renew getMovies method to use ArrayList

--- Spring/src/main/java/lt/techin/demo/controllers/movieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/movieController.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class movieController {
 
+    private ArrayList<String> movies = new ArrayList<>(
+            List.of("ToyStory", "A Bug's Life", "Toy Story 2", "Monsters, Inc.", "Finding Nemo"));
+
 
     @GetMapping("/movies")
-    public String getMovie() {
-        return"Toy Story 1";
+    public ArrayList<String> getMovies() {
+        return movies;
     }
 }
