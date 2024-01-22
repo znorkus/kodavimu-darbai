271ac92889f002de89dd64c7d00b653ab63201df
Zygimantas <zyg.norkus@gmail.com>
2024-01-22 09:45:52 +0200

    Remove redundant import from list

--- Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -2,7 +2,6 @@ package lt.techin.demo.controllers;
 
 
 import lt.techin.demo.models.Actor;
-import lt.techin.demo.models.Movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
