1886afa082df818c0a7cf38769051a882cb03498
Zygimantas <zyg.norkus@gmail.com>
2024-01-22 09:40:20 +0200

    Create ActorController controller

---
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -0,0 +1,33 @@
+package lt.techin.demo.controllers;
+
+
+import lt.techin.demo.models.Actor;
+import lt.techin.demo.models.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ActorController {
+
+    private ArrayList<Actor> actors = new ArrayList<>(
+            List.of(new Actor(1L, "Tim Allen", (short) 55,
+                            (short) 180, 100000L),
+                    new Actor(2L, "Tom Hanks", (short) 57,
+                            (short) 182, 120000L)));
+
+
+    @GetMapping("/movies")
+    public ArrayList<Actor> getMovies() {
+        return this.actors;
+    }
+
+    @GetMapping("/movies/{index}")
+    public Actor getMovie(@PathVariable int index) {
+
+        return this.actors.get(index);
+    }
+}
