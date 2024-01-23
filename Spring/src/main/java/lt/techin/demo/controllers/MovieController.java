064bfc425ce7b560109bc0f5106a07d67c81722e
Zygimantas <zyg.norkus@gmail.com>
2024-01-23 16:04:03 +0200

    Update MovieController with @PostMapping method

--- Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
@@ -4,9 +4,7 @@ package lt.techin.demo.controllers;
 import lt.techin.demo.models.Movie;
 import lt.techin.demo.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,6 +24,11 @@ public class MovieController {
         return this.movieRepository.findById(id).orElseThrow();
     }
 
+    @PostMapping("/movies")
+    public void insertMovie(@RequestBody Movie movie) {
+        this.movieRepository.save(movie);
+    }
+}
 //    @GetMapping("/movies")
 //    public List<Movie> getMovies() {
 //        return this.movieRepository.findAll();
@@ -35,4 +38,4 @@ public class MovieController {
 //    public Movie getMovie(@PathVariable int index) {
 //        return null;
 //    }
-}
+
