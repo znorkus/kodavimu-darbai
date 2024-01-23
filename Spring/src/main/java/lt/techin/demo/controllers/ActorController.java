4336f7cc9cfb65555bdea051da6184f00bc8e67e
Zygimantas <zyg.norkus@gmail.com>
2024-01-23 16:07:29 +0200

    Update ActorController with @PostMapping method

--- Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -5,9 +5,7 @@ import lt.techin.demo.models.Actor;
 import lt.techin.demo.models.Movie;
 import lt.techin.demo.repositories.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,16 +20,21 @@ public class ActorController {
     }
 
 
-//    @GetMapping("/actors")
-//    public List<Actor> getActors() {
-//
-//        return this.actorRepository.findAll();
-//    }
-
     @GetMapping("/actors/{id}")
     public Actor getActor(@PathVariable long id) {
         return this.actorRepository.findById(id).orElseThrow();
     }
+
+    @PostMapping("/actors")
+    public void insertActor(@RequestBody Actor actor) {
+        this.actorRepository.save(actor);
+    }
+
+    //    @GetMapping("/actors")
+//    public List<Actor> getActors() {
+//
+//        return this.actorRepository.findAll();
+//    }
 //
 //    @GetMapping("/actors/{index}")
 //    public Actor getActors(@PathVariable int index) {
