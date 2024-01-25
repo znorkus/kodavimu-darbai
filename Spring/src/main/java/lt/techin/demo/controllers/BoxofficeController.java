537411a65935c3895360def279877797c390f006
Zygimantas <zyg.norkus@gmail.com>
2024-01-25 12:01:35 +0200

    Create BoxofficeController

---
+++ Spring/src/main/java/lt/techin/demo/controllers/BoxofficeController.java
@@ -0,0 +1,34 @@
+package lt.techin.demo.controllers;
+
+
+import lt.techin.demo.models.Boxoffice;
+import lt.techin.demo.models.Review;
+import lt.techin.demo.repositories.BoxofficeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BoxofficeController {
+
+    private final BoxofficeRepository boxofficeRepository;
+
+    @Autowired
+    public BoxofficeController(BoxofficeRepository boxofficeRepository) {
+        this.boxofficeRepository = boxofficeRepository;
+    }
+
+    @GetMapping("/boxoffice")
+    public List<Boxoffice> getMovies() {
+        return this.boxofficeRepository.findAll();
+    }
+
+    @GetMapping("/boxoffice/{id}")
+    public Boxoffice getBoxoffice(@PathVariable long id) {
+        return this.boxofficeRepository.findById(id).orElseThrow();
+    }
+
+}
