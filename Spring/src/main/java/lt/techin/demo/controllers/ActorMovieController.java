d6b93bbeb10fb2965b169fbe8393db1f61cdf26f
Zygimantas <zyg.norkus@gmail.com>
2024-01-30 15:36:41 +0200

    Correct ActorMovieController

---
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorMovieController.java
@@ -0,0 +1,41 @@
+package lt.techin.demo.controllers;
+
+
+import lt.techin.demo.models.Actor;
+import lt.techin.demo.models.ActorMovie;
+import lt.techin.demo.models.ActorMovieId;
+import lt.techin.demo.models.Movie;
+import lt.techin.demo.repositories.ActorMovieRepository;
+import lt.techin.demo.services.ActorService;
+import lt.techin.demo.services.MovieService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ActorMovieController {
+
+    private final ActorMovieRepository actorMovieRepository;
+    private final ActorService actorService;
+    private final MovieService movieService;
+
+    public ActorMovieController(ActorMovieRepository actorMovieRepository,
+                                ActorService actorService, MovieService movieService) {
+        this.actorMovieRepository = actorMovieRepository;
+        this.actorService = actorService;
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/actors/{actorId}/movies/{movieId}")
+    public ActorMovie getActorMovie(@PathVariable("actorId") long actorId,
+                                    @PathVariable("movieId") long movieId) {
+        Actor actor = this.actorService.findActorById(actorId);
+        Movie movie = this.movieService.findMovieById(movieId);
+        ActorMovieId actorMovieId = new ActorMovieId(actor, movie);
+
+        return this.actorMovieRepository.findById(actorMovieId).orElse(null);
+    }
+
+    @PostMapping("/actormovies")
+    public ActorMovie saveActorMovie(@RequestBody ActorMovie actorMovie) {
+        return this.actorMovieRepository.save(actorMovie);
+    }
+}
