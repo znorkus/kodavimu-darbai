8fa76b0dcfc73b4aabf57d1895e05392f250bf9f
Zygimantas <zyg.norkus@gmail.com>
2024-01-30 09:46:56 +0200

    Repair '@PostMapping' method

--- Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -33,8 +33,8 @@ public class ActorController {
     }
 
     @PostMapping("/actors")
-    public void insertActor(@RequestBody Actor actor) {
-        this.actorService.saveActor(actor);
+    public Actor insertActor(@RequestBody Actor actor) {
+        return this.actorService.saveActor(actor);
     }
 
     @PutMapping("/actors/{id}")
