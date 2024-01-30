a8a87187881189d2f0b2de50b6778da52ec0f45a
Zygimantas <zyg.norkus@gmail.com>
2024-01-30 09:22:49 +0200

    Modify ActorController.java

--- Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -2,51 +2,59 @@ package lt.techin.demo.controllers;
 
 
 import lt.techin.demo.models.Actor;
+import lt.techin.demo.models.Movie;
 import lt.techin.demo.repositories.ActorRepository;
+import lt.techin.demo.services.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-//import java.util.List;
+import java.util.List;
 
 @RestController
 public class ActorController {
 
-    private final ActorRepository actorRepository;
+    private final ActorService actorService;
+
 
     @Autowired
-    public ActorController(ActorRepository actorRepository) {
-        this.actorRepository = actorRepository;
+    public ActorController(ActorService actorService) {
+        this.actorService = actorService;
+    }
+
+    @GetMapping("/actors")
+    public List<Actor> getActors() {
+        return this.actorService.getAllActors();
     }
 
 
     @GetMapping("/actors/{id}")
     public Actor getActor(@PathVariable long id) {
-        return this.actorRepository.findById(id).orElseThrow();
+        return this.actorService.findActorById(id);
     }
 
     @PostMapping("/actors")
     public void insertActor(@RequestBody Actor actor) {
-        this.actorRepository.save(actor);
+        this.actorService.saveActor(actor);
     }
 
     @PutMapping("/actors/{id}")
     public Actor updateActor(@RequestBody Actor actor, @PathVariable long id) {
-        if (this.actorRepository.existsById(id)) {
-            Actor actorFromDb = this.actorRepository.findById(id).orElseThrow();
+        if (this.actorService.existsActorById(id)) {
+            Actor actorFromDb = this.actorService.findActorById(id);
 
             actorFromDb.setName(actor.getName());
             actorFromDb.setAge(actor.getAge());
             actorFromDb.setHeight(actor.getHeight());
             actorFromDb.setSalary(actor.getSalary());
 
-            return this.actorRepository.save(actorFromDb);
+            return this.actorService.saveActor(actorFromDb);
         }
 
-        return this.actorRepository.save(actor);
+        return this.actorService.saveActor(actor);
     }
 
     @DeleteMapping("/actors/{id}")
     public void deleteActor(@PathVariable long id) {
-        this.actorRepository.deleteById(id);
+        this.actorService.deleteActorById(id);
     }
 }
