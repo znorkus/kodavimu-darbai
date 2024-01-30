c780a936efdddeebce8246903559731e4b20e140
Zygimantas <zyg.norkus@gmail.com>
2024-01-30 09:48:38 +0200

    Add 'insertActor_whenSaveActor_thenReturnIt' test

--- Spring/src/test/java/lt/techin/demo/ActorControllerTest.java
+++ Spring/src/test/java/lt/techin/demo/ActorControllerTest.java
@@ -1,5 +1,6 @@
 package lt.techin.demo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lt.techin.demo.controllers.ActorController;
 import lt.techin.demo.models.Actor;
 import lt.techin.demo.services.ActorService;
@@ -7,13 +8,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,5 +49,27 @@ public class ActorControllerTest {
                 .andExpect(jsonPath("$[1].salary").value(30000));
 
         verify(this.actorService).getAllActors();
+    }
+
+    @Test
+    void insertActor_whenSaveActor_thenReturnIt() throws Exception {
+        //given
+        Actor actor = new Actor("Tom Hanks", (short) 65, (short) 180, 100000L);
+        given(this.actorService.saveActor(any(Actor.class))).willReturn(actor);
+
+        //when
+        mockMvc.perform(post("/actors")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(actor)))
+
+                //then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Tom Hanks"))
+                .andExpect(jsonPath("$.age").value(65))
+                .andExpect(jsonPath("$.height").value(180))
+                .andExpect(jsonPath("$.salary").value(100000));
+
+        verify(this.actorService).saveActor(any(Actor.class));
     }
 }
