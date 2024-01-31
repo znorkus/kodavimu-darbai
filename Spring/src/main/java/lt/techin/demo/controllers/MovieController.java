ef3ed03f6f1766555c1f4362cec5555966a9fdb3
Zygimantas <zyg.norkus@gmail.com>
2024-01-31 09:57:54 +0200

    Update @PostMapping method

--- Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/MovieController.java
@@ -3,7 +3,9 @@ package lt.techin.demo.controllers;
 import lt.techin.demo.models.Movie;
 import lt.techin.demo.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -28,9 +30,18 @@ public class MovieController {
     }
 
     @PostMapping("/movies")
-    public Movie insertMovie(@RequestBody Movie movie) {
-        return this.movieService.saveMovie(movie);
+    public ResponseEntity<Movie> insertMovie(@RequestBody Movie movie) {
+        Movie savedMovie = this.movieService.saveMovie(movie);
+
+        return ResponseEntity
+                .created(ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}").buildAndExpand(savedMovie.getId())
+                        .toUri())
+                .body(savedMovie);
     }
+//    public Movie insertMovie(@RequestBody Movie movie) {
+//        return this.movieService.saveMovie(movie);
+//    }
 
     @PutMapping("/movies/{id}")
     public Movie updateMovie(@RequestBody Movie movie, @PathVariable long id) {
