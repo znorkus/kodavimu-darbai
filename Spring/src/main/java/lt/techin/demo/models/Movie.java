96affb3b0873322332f333751a12c847200631d9
Zygimantas <zyg.norkus@gmail.com>
2024-01-31 15:09:20 +0200

    Add custom validations to Movie.java

--- Spring/src/main/java/lt/techin/demo/models/Movie.java
+++ Spring/src/main/java/lt/techin/demo/models/Movie.java
@@ -1,6 +1,9 @@
 package lt.techin.demo.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "Movies")
@@ -9,9 +12,16 @@ public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @NotNull
+    @Size(min = 1, message = "Title should be at least 1 characters long")
     private String title;
+    @NotNull
+    @Size(min = 3, message = "Directors name should be at least 3 characters long")
     private String director;
+    @NotNull
+    @Min(value = 1900, message = "Year release should not be older than 1900")
     private short yearRelease;
+    @Min(value = 30, message = "Length minutes should not be lower than 30 minutes")
     private short lengthMinutes;
 
     public Movie(String title, String director, short yearRelease,
