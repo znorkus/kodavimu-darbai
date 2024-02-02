3cca3674921e26db8448e3d1b762aff678e46d30
Zygimantas <zyg.norkus@gmail.com>
2024-02-02 15:48:05 +0200

    Add Optional method

--- Spring/src/main/java/lt/techin/demo/repositories/UserRepository.java
+++ Spring/src/main/java/lt/techin/demo/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package lt.techin.demo.repositories;
 import lt.techin.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUsername(String username);
 }
