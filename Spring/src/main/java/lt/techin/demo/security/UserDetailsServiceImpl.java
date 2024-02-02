1833961871fbeeaf1da169c803c64e48947221b7
Zygimantas <zyg.norkus@gmail.com>
2024-02-02 15:58:24 +0200

    Create UserDetailServiceImpl

---
+++ Spring/src/main/java/lt/techin/demo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package lt.techin.demo.security;
+
+import lt.techin.demo.models.User;
+import lt.techin.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username).orElse(null);
+
+        return user;
+    }
+}
