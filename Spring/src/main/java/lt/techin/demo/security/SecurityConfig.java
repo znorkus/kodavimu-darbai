519d2bc004d1dec49c756f9861b4f4202f4aaf8d
Zygimantas <zyg.norkus@gmail.com>
2024-02-02 12:41:50 +0200

    Create SecurityConfig configuration file

---
+++ Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package lt.techin.demo.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("ziggy")
+                .password("123456")
+                .authorities("ADMIN")
+                .build();
+
+        userDetailsManager.createUser(user);
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
