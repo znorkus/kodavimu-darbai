d127fc6829eb67627b4ac342ef0b854bdbd6651b
Zygimantas <zyg.norkus@gmail.com>
2024-02-05 10:44:22 +0200

    Add SecurityFilterChain method

--- Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
+++ Spring/src/main/java/lt/techin/demo/security/SecurityConfig.java
@@ -3,17 +3,20 @@ package lt.techin.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
+    //    @Bean
 //    public UserDetailsService userDetailsService() {
 //        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
 //
@@ -26,6 +29,16 @@ public class SecurityConfig {
 //
 //        return userDetailsManager;
 //    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .csrf(c -> c.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
