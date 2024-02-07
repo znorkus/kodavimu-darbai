ad637998c95956d08b9a7f7c8498692511835bca
Zygimantas <zyg.norkus@gmail.com>
2024-02-07 13:24:54 +0200

    Corret DirectorRepository.java

--- Spring/src/main/java/lt/techin/demo/repositories/DirectorRepository.java
+++ Spring/src/main/java/lt/techin/demo/repositories/DirectorRepository.java
@@ -1,7 +1,7 @@
 package lt.techin.demo.repositories;
 
-import lt.techin.demo.models.Actor;
+import lt.techin.demo.models.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DirectorRepository extends JpaRepository<Actor, Long> {
+public interface DirectorRepository extends JpaRepository<Director, Long> {
 }
