ccc94975a5ffc194710e7260b148bec462cf7bf2
Zygimantas <zyg.norkus@gmail.com>
2024-02-09 14:52:22 +0200

    Correct data type from 'let' to 'const'

--- JavaScript/1 Tema/script.js
+++ JavaScript/1 Tema/script.js
@@ -1,12 +1,12 @@
 //1
 
 function daysToHoursAndMinutes(days) {
-  let hours = days * 24;
-  let minutes = hours * 60;
+  const hours = days * 24;
+  const minutes = hours * 60;
   return `${days} Days is ${hours} Hours or ${minutes} Minutes`;
 }
-let days = 5;
-let result = daysToHoursAndMinutes(days);
+const days = 5;
+const result = daysToHoursAndMinutes(days);
 console.log(result);
 
 //2
