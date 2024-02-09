a062d22dbb1a6486908d7dace41271f5166ceb76
Zygimantas <zyg.norkus@gmail.com>
2024-02-09 14:46:02 +0200

    Correct first excercise

--- JavaScript/1 Tema/script.js
+++ JavaScript/1 Tema/script.js
@@ -1,5 +1,12 @@
 //1
 
-const days = 3;
-const hours = days * 24;
-console.log(hours + " hours");
+function daysToHoursAndMinutes(days) {
+  let hours = days * 24;
+  let minutes = hours * 60;
+  return `${days} Days is ${hours} Hours or ${minutes} Minutes`;
+}
+let days = 5;
+let result = daysToHoursAndMinutes(days);
+console.log(result);
+
+//2
