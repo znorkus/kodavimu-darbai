375c5b1377ed91a2f9911d09d8ac7426e0d0230f
Zygimantas <zyg.norkus@gmail.com>
2024-02-12 13:23:30 +0200

    Do first excercise

---
+++ JavaScript/1 Tema/01Praktinis/script.js
@@ -0,0 +1,14 @@
+function changeColorToYellow() {
+  const p = document.querySelector("p");
+  p.style.color = "yellow";
+}
+
+function changeColorToGreen() {
+  const p = document.querySelector("p");
+  p.style.color = "green";
+}
+
+function changeColorToRed() {
+  const p = document.querySelector("p");
+  p.style.color = "red";
+}
