2ce4b1e864fbfc01a75fa25c18be065f88ad45f8
Zygimantas <zyg.norkus@gmail.com>
2024-02-12 11:20:16 +0200

    Do practise excercise

---
+++ JavaScript/Praktika 0/script.js
@@ -0,0 +1,33 @@
+// const logo = document.getElementsByClassName(
+//   "navbar-brand navbar-brand-center d-flex align-items-center p-0 only-mobile"
+// );
+// console.log(logo);
+
+const pageName = document.getElementsByClassName("display-4");
+console.log(pageName);
+
+// const pageName = document.querySelector("h1");
+// console.log(pageName);
+
+const button = document.getElementsByClassName(
+  "btn btn-primary btn-shadow btn-lg"
+);
+console.log(button);
+
+const btn = document.querySelector(
+  "body > div.boxed-page > div > div > div > div.col-lg-6.hero-left > div > a.btn.btn-primary.btn-shadow.btn-lg"
+);
+console.log(button);
+
+const supportHours = document.querySelector(
+  "body > div.boxed-page > div > div > div > div.col-lg-6.hero-left > ul > li:nth-child(3)"
+);
+console.log(supportHours);
+
+const teamPageButton = document.querySelector(
+  "#navbarSupportedContent > ul:nth-child(3) > div > li:nth-child(2) > a"
+);
+console.log(teamPageButton);
+
+const searchPannel = document.querySelector("#side-search-open");
+console.log(searchPannel);
