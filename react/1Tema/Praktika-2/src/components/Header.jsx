a7512f3ad825d9276572d3e2b78a5d7bd0161248
Zygimantas <zyg.norkus@gmail.com>
2024-02-14 14:52:33 +0200

    Correct Header.jsx

--- react/1Tema/Praktika-2/src/components/Header.jsx
+++ react/1Tema/Praktika-2/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <img
         src="https://images.pexels.com/photos/300857/pexels-photo-300857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="View of lake in winter with ice on top and mountains in horizon"
-        className="Header__img w-100 h-55 object-fit-cover"
+        className="header__img w-100 object-fit-cover mb-3"
       ></img>
     </div>
   );
