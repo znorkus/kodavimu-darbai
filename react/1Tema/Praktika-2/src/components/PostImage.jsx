ed04ec36e1cb70172a7426ba3116455eff951009
Zygimantas <zyg.norkus@gmail.com>
2024-02-14 14:53:44 +0200

    Correct PostImage.jsx

--- react/1Tema/Praktika-2/src/components/PostImage.jsx
+++ react/1Tema/Praktika-2/src/components/PostImage.jsx
@@ -4,9 +4,7 @@ export default function PostImage() {
       <img
         src="https://images.pexels.com/photos/849835/pexels-photo-849835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="Photo of old blue car with stuff on roof in winter"
-        width="500"
-        height="300"
-        className="rounded-circle"
+        className="rounded-circle img-fluid"
       />
     </>
   );
