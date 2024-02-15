eaf24b336704278aee2a01be89d95f950bb3c40f
Zygimantas <zyg.norkus@gmail.com>
2024-02-15 16:13:17 +0200

    Do first excercise and change files directory

---
+++ react/2Tema/Praktika-1/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [btnPress, setBtnPress] = useState(false);
+  function changeBtnPress() {
+    setBtnPress(true);
+  }
+  return (
+    <>
+      <div
+        className="card"
+        style={{ width: "18rem" }}
+      >
+        <div className="card-body">
+          <h5 className={btnPress ? "text-success" : "text-danger"}>
+            Task is {btnPress ? "DONE!" : "not complete!"}
+          </h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <button
+            onClick={changeBtnPress}
+            type="button"
+            className={btnPress ? "btn btn-success" : "btn btn-danger"}
+          >
+            {btnPress ? "DONE" : "Mark as DONE"}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
