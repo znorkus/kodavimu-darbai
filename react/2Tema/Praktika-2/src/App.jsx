a90939965dc9da7ba170a9a9b0cfd4f155c65032
Zygimantas <zyg.norkus@gmail.com>
2024-02-19 10:41:35 +0200

    Remove redundant commented code

--- react/2Tema/Praktika-2/src/App.jsx
+++ react/2Tema/Praktika-2/src/App.jsx
@@ -58,29 +58,3 @@ export default function App() {
     </div>
   );
 }
-
-//   return (
-//     <>
-//       <div
-//         className="card"
-//         style={{ width: "18rem" }}
-//       >
-//         <div className="card-body">
-//           <h5 className={btnPress ? "text-success" : "text-danger"}>
-//             Task is {btnPress ? "DONE!" : "not complete!"}
-//           </h5>
-//           <p className="card-text">
-//             Some quick example text to build on the card title and make up the
-//             bulk of the card's content.
-//           </p>
-//           <button
-//             onClick={changeBtnPress}
-//             type="button"
-//             className={`btn ${btnPress ? "btn-success" : "btn-danger"}`}
-//           >
-//             {btnPress ? "DONE" : "Mark as DONE"}
-//           </button>
-//         </div>
-//       </div>
-//     </>
-//   );
