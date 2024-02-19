ec9d7c0aeafe9f095851edc3c64ea3a10383ac2b
Zygimantas <zyg.norkus@gmail.com>
2024-02-19 10:37:16 +0200

    Do second excercise

---
+++ react/2Tema/Praktika-2/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [pressLike, setBtnLike] = useState(0);
+  const [pressHate, setBtnHate] = useState(0);
+
+  function countLikes() {
+    setBtnLike((pressLike) => pressLike + 1);
+  }
+
+  function countHates() {
+    setBtnHate((pressHate) => pressHate + 1);
+  }
+
+  function resetCounters() {
+    setBtnLike((pressLike) => 0);
+    setBtnHate((pressHate) => 0);
+  }
+
+  return (
+    <div className="card">
+      <img
+        src="https://miro.medium.com/v2/resize:fit:1200/1*hm7aE3BdUfUWUgBYK1GiZA.jpeg"
+        className="card-body"
+        alt="React conding language logo with How I fell in love with React text"
+      />
+      <div className="card-body">
+        <h5 className="card-title">Post</h5>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          culpa adipisci ex velit saepe maiores, dolores eos ipsam, veniam
+          repudiandae deleniti voluptate itaque, accusamus fugit! Exercitationem
+          qui officia illum tempore.
+        </p>
+        <button
+          onClick={countLikes}
+          type="button"
+          className="btn btn-success"
+        >
+          Likes {pressLike}
+        </button>
+        <button
+          onClick={countHates}
+          type="button"
+          className="btn btn-danger"
+        >
+          Hates {pressHate}
+        </button>
+        <button
+          onClick={resetCounters}
+          type="button"
+          className="btn btn-primary"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+//   return (
+//     <>
+//       <div
+//         className="card"
+//         style={{ width: "18rem" }}
+//       >
+//         <div className="card-body">
+//           <h5 className={btnPress ? "text-success" : "text-danger"}>
+//             Task is {btnPress ? "DONE!" : "not complete!"}
+//           </h5>
+//           <p className="card-text">
+//             Some quick example text to build on the card title and make up the
+//             bulk of the card's content.
+//           </p>
+//           <button
+//             onClick={changeBtnPress}
+//             type="button"
+//             className={`btn ${btnPress ? "btn-success" : "btn-danger"}`}
+//           >
+//             {btnPress ? "DONE" : "Mark as DONE"}
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
