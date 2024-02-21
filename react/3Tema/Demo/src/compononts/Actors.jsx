a68bfd049a57f0d3b58832e2745be66a6f057b3c
Zygimantas <zyg.norkus@gmail.com>
2024-02-21 14:43:09 +0200

    Beutifi form look

--- react/3Tema/Demo/src/compononts/Actors.jsx
+++ react/3Tema/Demo/src/compononts/Actors.jsx
@@ -25,55 +25,62 @@ export default function Actors() {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        defaultValue="Name"
-        {...register("name", { required: true, minLength: 2 })}
-      />
-      {errors.name && errors.name.type === "required" && (
-        <span>Name field is required</span>
-      )}
-      {errors.name && errors.name.type === "minLength" && (
-        <span>Name must be at least 2 characters long</span>
-      )}
-
-      <input
-        defaultValue="Age"
-        type="number"
-        {...register("age", { required: true, min: 5 })}
-      />
-      {errors.name && errors.name.type === "required" && (
-        <span>Age field is required</span>
-      )}
-      {errors.age && errors.age.type === "min" && (
-        <span>Actor must be at least 5 year old</span>
-      )}
-
-      <input
-        type="number"
-        defaultValue="Height"
-        {...register("height", { required: true, min: 50 })}
-      />
-      {errors.height && errors.height.type === "required" && (
-        <span>Height field is required</span>
-      )}
-      {errors.height && errors.height.type === "min" && (
-        <span>Actor must be at least 50 centimeters tall</span>
-      )}
-
-      <input
-        type="number"
-        defaultValue="Salary"
-        {...register("salary", { required: true, min: 1 })}
-      />
-      {errors.salary && errors.salary.type === "required" && (
-        <span>Salary field is required</span>
-      )}
-      {errors.salary && errors.salary.type === "min" && (
-        <span>Actor's salary must be at least 1usd.</span>
-      )}
-
-      <input type="submit" />
-    </form>
+    <div>
+      POST TO ACTORS DATABASE
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Name"
+          // defaultValue="Name"
+          {...register("name", { required: true, minLength: 2 })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <span>Name field is required</span>
+        )}
+        {errors.name && errors.name.type === "minLength" && (
+          <span>Name must be at least 2 characters long</span>
+        )}
+        <br></br>
+        <input
+          type="number"
+          // defaultValue="10"
+          placeholder="Age"
+          {...register("age", { required: true, min: 5 })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <span>Age field is required</span>
+        )}
+        {errors.age && errors.age.type === "min" && (
+          <span>Actor must be at least 5 year old</span>
+        )}
+        <br></br>
+        <input
+          type="number"
+          // defaultValue="10"
+          placeholder="Height"
+          {...register("height", { required: true, min: 50 })}
+        />
+        {errors.height && errors.height.type === "required" && (
+          <span>Height field is required</span>
+        )}
+        {errors.height && errors.height.type === "min" && (
+          <span>Actor must be at least 50 centimeters tall</span>
+        )}
+        <br></br>
+        <input
+          type="number"
+          // defaultValue="10"
+          placeholder="Salary"
+          {...register("salary", { required: true, min: 1 })}
+        />
+        {errors.salary && errors.salary.type === "required" && (
+          <span>Salary field is required</span>
+        )}
+        {errors.salary && errors.salary.type === "min" && (
+          <span>Actor's salary must be at least 1usd.</span>
+        )}
+        <br></br>
+        <input type="submit" />
+      </form>
+    </div>
   );
 }
