b22d622272371fbec3d15a516b0073edd836ca3c
Zygimantas <zyg.norkus@gmail.com>
2024-02-21 13:09:03 +0200

    Add Cross Origin annotation

--- Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ActorController {
 
     private final ActorService actorService;
