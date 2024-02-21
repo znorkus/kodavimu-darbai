2e104c3ba8cabb12837b38bf3dc1794d0cf9efb2
Zygimantas <zyg.norkus@gmail.com>
2024-02-21 13:18:39 +0200

    Correct ActroController CrossOrigin annotation

--- Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
+++ Spring/src/main/java/lt/techin/demo/controllers/ActorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173 Cro")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ActorController {
 
     private final ActorService actorService;
