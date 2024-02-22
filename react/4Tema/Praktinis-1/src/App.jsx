07695d62abf4cce01e045fc536072f1158afd248
Zygimantas <zyg.norkus@gmail.com>
2024-02-22 11:09:08 +0200

    Add ErrorPage Route and NavLink

--- react/4Tema/Praktinis-1/src/App.jsx
+++ react/4Tema/Praktinis-1/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, NavLink } from "react-router-dom";
 import Movies from "./components/Movies";
 import Actors from "./components/Actors";
+import ErrorPage from "./components/ErrorPage";
 export default function App() {
   return (
     <>
@@ -12,6 +13,9 @@ export default function App() {
             </li>
             <li>
               <NavLink to="/movies">Movies</NavLink>
+            </li>
+            <li>
+              <NavLink to="/error">Error Page</NavLink>
             </li>
           </ul>
         </nav>
@@ -25,6 +29,10 @@ export default function App() {
           <Route
             path="/actors"
             element={<Actors />}
+          />
+          <Route
+            path="/error"
+            element={<ErrorPage />}
           />
         </Routes>
       </main>
